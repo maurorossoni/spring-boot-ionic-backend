bc49d6d0bd70946fb521819637a546bc09c598d3
Mauro Rossoni <junior.rossonibn@gmail.com>
2023-03-27 19:50:19 -0300

    End-point pedidos/ {id} diSponibilizado

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -3,6 +3,8 @@ package com.nelioalves.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 
@@ -11,8 +13,10 @@ public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
+	
 	
 	private Double desconto;
 	private Integer quantidade;
@@ -29,12 +33,11 @@ public class ItemPedido implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-	
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
-	
-	public Produto getProduto() {
+		public Produto getProduto() {
 		return id.getProduto();
 	}
 
---
+++ src/main/java/com/nelioalves/cursomc/services/PedidoService.java
@@ -0,0 +1,34 @@
+package com.nelioalves.cursomc.services;
+
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Pedido;
+import com.nelioalves.cursomc.repositories.PedidoRepository;
+import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+
+
+@Service
+public class PedidoService {
+
+   @Autowired
+	private PedidoRepository repo;
+	
+
+		 public Optional<Pedido> buscar(Integer id) {
+			 Optional<Pedido> obj = repo.findById(id);
+			 if (obj == null) {
+				 throw new ObjectNotFoundException("Objeto não encontrado! ID: " + id
+						 + ", Tipo: " + Pedido.class.getName());
+			 }
+              return obj;
+		
+	}
+	
+}
